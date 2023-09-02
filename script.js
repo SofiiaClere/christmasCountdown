38fb4f28bd245be9a9083cd3750c873108e81bff
Glitch (glitch-hello-website) <none>
2023-09-02 20:33:47 +0000

    🚃👣 Checkpoint ./index.html:59895810/7338 ./script.js:59895810/2607 ./style.css:59895810/5971

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+function christmasCountdown(){
+    const ChristmasDate = new Date("December 25, 2023 00:00");
+    const now = new Date();
+    const diff = ChristmasDate - now;
+    
+    const msInSeconds = 1000;
+    const msInMinutes = 60000;
+    const msInHour = 3600000;
+    const msInDay = 86400000;
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+    const displayDay = Math.floor((diff/msInDay));
+    document.querySelector(".days").textContent = displayDay;
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
+    const displayHours = Math.floor((diff%msInDay)/msInHour);
+    document.querySelector(".hours").textContent = displayHours;
+
+    const displayMinutes = Math.floor((diff%msInHour)/msInMinutes);
+    document.querySelector(".minutes").textContent = displayMinutes;
+
+    const displaySeconds = Math.floor((diff%msInMinutes)/msInSeconds);
+    document.querySelector(".seconds").textContent = displaySeconds;
+
+    if (diff<= 0){
+        document.querySelector(".days").textContent = 0;
+        document.querySelector(".hours").textContent = 0;
+        document.querySelector(".minutes").textContent = 0;
+        document.querySelector(".seconds").textContent = 0;
+        clearInterval(timerId);
+        merryChristmas();
+    }
+    
+}
+
+let timerId = setInterval(christmasCountdown,1000);
+
+function merryChristmas(){
+    const heading = document.querySelector(".header");
+    heading.textContent="Merry Christmas!!!";
+    heading.classList.add("red");
 }
 
 
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
+const play = document.querySelector("#btn")
 
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
-});
+play.addEventListener("click", function(){
+    document.querySelector("#myAudio").play();
+})
+
+
+
